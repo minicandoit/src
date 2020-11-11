66188076eaef9eb156d61100ededcb15be913896
minicandoit <74155239+minicandoit@users.noreply.github.com>
2020-11-11 16:19:25 -0500

    officeHourCode more with git class

---
+++ src/Office_Hours/Practice_11_11_2020/ArraysPractice3.java
@@ -0,0 +1,38 @@
+package Office_Hours.Practice_11_11_2020;
+
+import java.util.Arrays;
+
+public class ArraysPractice3 {
+    public static void main(String[] args) {
+        //anagram task==>same length of string containing same character
+
+      String str1="abcdefg";
+      String str2="GFEDCBA";
+
+     // str1=str1.toLowerCase();
+      //str2=str2.toLowerCase();
+      String []arr1=str1.toLowerCase().split("");
+      String []arr2=str2.toLowerCase().split("");
+
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+
+boolean isAnagram= Arrays.equals(arr1,arr2);
+        System.out.println(isAnagram);//true
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Office_Hours/Practice_11_11_2020/MultidimensionalArrays.java
@@ -0,0 +1,45 @@
+package Office_Hours.Practice_11_11_2020;
+
+import java.util.Arrays;
+
+public class MultidimensionalArrays {
+    public static void main(String[] args) {
+        int[] arr1D={1,2,3,4};
+        //           0 1 2 3
+        //               0 1 2 3   0 1 2   0 1
+        int[][]  arr2D={{1,2,3,4},{5,6,7},{8,9}};//count how many single array
+        //                0          1      2
+
+  //{8,9}
+        System.out.println(Arrays.toString(arr2D[2]));
+        System.out.println(Arrays.deepToString(arr2D));
+
+        System.out.println("==================================");
+
+        for(int each1DArray []:arr2D){
+         //   System.out.println(Arrays.toString(each1DArray));
+            for(int eachElement:each1DArray){
+                //System.out.println(eachElement);
+                if(eachElement%2==0){
+                    System.out.println(eachElement);
+                }
+            }
+        }
+
+        System.out.println("========================");
+
+for(int[] each1DArray:arr2D){
+    for(int eachElement:each1DArray){
+        if(eachElement%2!=0){
+            System.out.print(eachElement+" ");
+        }
+    }
+}
+
+
+
+
+
+
+    }
+}
